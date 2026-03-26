6fdf3cb925a80610626be8952c6f9ef0d84c2e54
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2026-03-25 20:15:39 -0600

    first commit

---
+++ src/main/java/com/example/practicae1lab/PracticaE1LabApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicae1lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaE1LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaE1LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicae1lab/PracticaE1LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicae1lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaE1LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
